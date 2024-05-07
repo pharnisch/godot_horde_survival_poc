f2e388feecb4cbb6252430e8bbc777adccab959d
Philipp <p.harnisch@tu-berlin.de>
2024-05-07 15:59:08 +0200

    .

--- world.gd
+++ world.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @export var adaptive = true
+var difficulty_level = 50
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,3 +11,11 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func change_difficulty_level(diff):
+	if adaptive:
+		difficulty_level += diff
+		difficulty_level = min(difficulty_level, 0)
+		difficulty_level = min(difficulty_level, 100)
+	else:
+		pass # don't change difficulty level for fixed mode
